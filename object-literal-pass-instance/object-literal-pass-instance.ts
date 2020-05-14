3eb6bcdf12b643e3c25b70d4a57b4da5ae27dda6
Aaron Turner <aaron@aaronthedev.com>
2020-05-14 10:25:45 -0700

    Wrote the passing an instance through an object literal test case

---
+++ object-literal-pass-instance/object-literal-pass-instance.ts
@@ -0,0 +1,42 @@
+import {Console} from "as-wasi";
+
+class OtherClass {
+  stuff: i32;
+
+  constructor(newStuff: i32) {
+    this.stuff = newStuff;
+  }
+}
+
+class MyClassInit {
+  otherClass: OtherClass;
+}
+
+class MyClass {
+  _myParam: i32;
+  _otherClass: OtherClass;
+
+  constructor(param: i32, init: MyClassInit) {
+    this._myParam = param;
+    this._otherClass = init.otherClass;
+  }
+
+  toString(): string {
+    return this._otherClass.stuff.toString();
+  }
+}
+
+export function _start(): void {
+
+  // Make our Other class
+  let otherClass = new OtherClass(24);
+
+  // Pass to my class as an object literal
+  let myClass = new MyClass(2424, {
+    otherClass: otherClass
+  });
+
+  Console.log("Logging myClass.toString() ...\n");
+  Console.log(myClass.toString());
+  Console.log("\n");
+}
